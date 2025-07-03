249be80c24fbe46984b647e44435cce21f4dffbe
Saida Mahmood <saida.mahmood04@gmail.com>
2025-07-02 18:15:03 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://farhinmahmood.github.io",
+  base: "/IBG-DFW/",
   plugins: [tailwindcss(), react()],
 });
